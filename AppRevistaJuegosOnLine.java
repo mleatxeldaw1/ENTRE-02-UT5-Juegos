264420f488caf81b5406c20eaafc56e19007825c
Mitxel <mleatxeldaw1@gmail.com>
2020-01-24 22:40:41 +0100

    Terrminado

--- AppRevistaJuegosOnLine.java
+++ AppRevistaJuegosOnLine.java
@@ -5,11 +5,25 @@
  */
 public class AppRevistaJuegosOnLine 
 {
-	public static void main(String[] args)
-	{
-		
+    public static void main(String[] args)
+    {
+        if(args.length != 2){
+            System.out.println("Error en nº argumentos, Sintaxis: java DemoCD <revista> <n>");
+        }else{
+            String nombre = args[0];
+            int juegos = Integer.parseInt(args[1]);
+            RevistaOnLineJuegos revista = new RevistaOnLineJuegos(nombre, juegos);
+            revista.leerDeFichero();
+            System.out.println(revista.toString());
+            revista.puntuar("Planet Zoo", 8);
+            revista.puntuar("Steep", 7);
+            revista.puntuar("Catastronauts", 9);
+            revista.puntuar("Wattam", 9);
+            System.out.println(revista.toString());
+            revista.valoracionMayorQue(8.2);
+            revista.borrarDeGenero(Genero.valueOf("ROL".toUpperCase()));
+            System.out.println(revista.toString());
 
-		 
-	}
-
+        }
+    }
 }
--- RevistaOnLineJuegos.java
+++ RevistaOnLineJuegos.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * La clase representa a una tienda on-line en la
@@ -12,127 +13,182 @@ import java.util.Scanner;
  */
 public class RevistaOnLineJuegos 
 {
-	private String nombre;
-	private Juego[] juegos;
-	private int total;
+    private String nombre;
+    private Juego[] juegos;
+    private int total;
 
-	/**
-	 * Constructor  
-	 * Crea el array de juegos al tamaño máximo indicado por la constante
-	 * e inicializa el resto de atributos
-	 */
-	public RevistaOnLineJuegos(String nombre, int n) {
-		 
-	}
+    /**
+     * Constructor  
+     * Crea el array de juegos al tamaño máximo indicado por la constante
+     * e inicializa el resto de atributos
+     */
+    public RevistaOnLineJuegos(String nombre, int n) {
+        this.nombre = nombre;
+        juegos = new Juego[n];
+        total = 0;
+    }
 
-	/**
-	 * Devuelve true si el array está completo, false en otro caso
-	 */
-	public boolean estaCompleta() {
-		 
-	}
+    /**
+     * Devuelve true si el array está completo, false en otro caso
+     */
+    public boolean estaCompleta() {
+        return total == juegos.length;
+    }
 
-	/**
-	 *    Añade un nuevo juego solo si el array no está completo y no existe otro juego
-	 *    ya con el mismo nombre.  Si no se puede añadir se muestra los mensajes adecuados 
-	 *    (diferentes en cada caso)
-	 *    
-	 *    El juego se añade de tal forma que queda insertado en orden alfabético de título
-	 *    (de menor a mayor)
-	 *     !!OJO!! No hay que ordenar ni utilizar ningún algoritmo de ordenación
-	 *    Hay que insertar en orden 
-	 *    
-	 */
-	public void add(Juego juego) {
-		 
+    /**
+     *    Añade un nuevo juego solo si el array no está completo y no existe otro juego
+     *    ya con el mismo nombre.  Si no se puede añadir se muestra los mensajes adecuados 
+     *    (diferentes en cada caso)
+     *    
+     *    El juego se añade de tal forma que queda insertado en orden alfabético de título
+     *    (de menor a mayor)
+     *     !!OJO!! No hay que ordenar ni utilizar ningún algoritmo de ordenación
+     *    Hay que insertar en orden 
+     *    
+     */
+    public void add(Juego juego) {
 
-	}
+        if(estaCompleta()){
+            System.out.println("La lista esta completa y no se pueden añadir mas juegos");
+        }
+        if(existeJuego(juego.getTitulo()) == 1){
+            System.out.println("El juego ya esta en la lista");
+        }
+        if(!estaCompleta() && existeJuego(juego.getTitulo()) == -1){
+            int posmin = 0;
+            if(total == 0){
+                juegos[total] = juego;
+                total++;
+            } else{
+                for(int aux = total -1; aux >= 0; aux--){
+                    if(juegos[aux].getTitulo().compareTo(juego.getTitulo()) > 0){
+                        juegos[aux +1] = juegos[aux];
+                    } else {
+                        juegos[aux + 1] = juego;
+                        total++;
+                    }
+                }
+                if(total == 0){
+                    juegos[0] = juego;
+                    total++;
+                }
+            }
+        }
+    }
 
-	 
+    /**
+     * Efectúa una búsqueda en el array del juego cuyo titulo se
+     * recibe como parámetro. Es ndiferente mayúsculas y minúsculas
+     * Si existe el juego devuelve su posición, si no existe devuelve -1
+     */
 
-	/**
-	 * Efectúa una búsqueda en el array del juego cuyo titulo se
-	 * recibe como parámetro. Es ndiferente mayúsculas y minúsculas
-	 * Si existe el juego devuelve su posición, si no existe devuelve -1
-	 */
-	public int existeJuego(String titulo) {
-		 
-	    
-		return 0;
+    public int existeJuego(String titulo) {
+        for(int i = 0 ; i < total; i++){
+            if(juegos[i].getTitulo().compareTo(titulo) >= 0){
+                return i;
+            }
 
-	}
+        }
+        return -1;
+    }
 
-	/**
-	* Representación textual de la revista
-	* Utiliza StringBuilder como clase de apoyo.
-	* Se incluye el nombre de la  revista on-line.
-	* (Ver resultados de ejecución)
-	*/
-	public String toString() {
-		
-	    
-	    
-		return "";
+    /**
+     * Representación textual de la revista
+     * Utiliza StringBuilder como clase de apoyo.
+     * Se incluye el nombre de la  revista on-line.
+     * (Ver resultados de ejecución)
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < total; i ++){
+            if(existeJuego(juegos[i].getTitulo()) >= 0){
+                sb.append("Ya esta publicado el juego "+juegos[i].getTitulo()+" en la revista"+nombre+"\n");
+            }
+        }
+        sb.append("Los mejores juegos en nuestra revista " + nombre +"(" + total + " juegos)\n");
+        for(int j = 0; j < total; j ++){
+            sb.append(juegos[j].toString() + "\n--------------------\n");
+        }
+        return sb.toString();
+    }
 
-	}
+    /**
+     *  Se puntúa el juego de título indicado con 
+     *  la puntuación recibida como parámetro. 
+     *  La puntuación es un valor entre 1 y 10 (asumimos esto como correcto)
+     *  Si el juego no existe se muestra un mensaje en pantalla
+     */
+    public void puntuar(String titulo, int puntuacion) {
+        int pos = 0;
+        if(existeJuego(titulo) > 0){
+            while(juegos[pos].getTitulo()!= titulo){
+                pos++;
+            }
+            juegos[pos].puntuar(puntuacion);  
+        }
+        else {
+            System.out.println("Puntuando...\n" + "No existe el juego"+titulo+"\n");
+        }
+    }
 
-	/**
-	 *  Se puntúa el juego de título indicado con 
-	 *  la puntuación recibida como parámetro. 
-	 *  La puntuación es un valor entre 1 y 10 (asumimos esto como correcto)
-	 *  Si el juego no existe se muestra un mensaje en pantalla
-	 */
-	public void puntuar(String titulo, int puntuacion) {
-		 
+    /**
+     * Devuelve un array con los nombres de los juegos 
+     * con una valoración media mayor a la indicada  
+     * 
+     * El array se devuelve todo en mayúsculas y ordenado ascendentemente
+     */
+    public String[] valoracionMayorQue(double valoracion) {
+        String[] lista = new String[total];
+        for(int i = 0; i < total; i++){
+            if(juegos[i].getValoracionMedia()>valoracion){
+                lista[i] += juegos[i].getTitulo();
+            }
+        }
+        Arrays.sort(lista);
 
-	}
+        return lista;
+    }
 
-	/**
-	* Devuelve un array con los nombres de los juegos 
-	* con una valoración media mayor a la indicada  
-	* 
-	* El array se devuelve todo en mayúsculas y ordenado ascendentemente
-	*/
-	public String[] valoracionMayorQue(double valoracion) {
-		 
-	    
-	    
-		return null;
+    /**
+     * Borrar los juegos del género indicado devolviendo
+     * el nº de juegos borradas
+     */
+    public int borrarDeGenero(Genero genero) {
+        int borrados = 0;
+        for(int i = 0; i < total;i ++){
+            if(genero.equals(juegos[i].getGenero())){
+                for(int j = i; j < total - 1; j ++){
+                    juegos[j] =juegos[j + 1];
+                }
+                i --;
+                borrados ++;
+                total--;
+            }
+        }
+        return borrados;
+    }
 
-	}
+    /**
+     * Lee de un fichero de texto los datos de los juegos
+     * con ayuda de un objeto de la  clase Scanner
+     * y los guarda en el array. 
+     */
+    public void leerDeFichero() {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("juegos.txt"));
+            while (sc.hasNextLine()) {
+                Juego juego = new Juego(sc.nextLine());
+                this.add(juego);
 
-	/**
-	 * Borrar los juegos del género indicado devolviendo
-	 * el nº de juegos borradas
-	 */
-	public int borrarDeGenero(Genero genero) {
-		
-	    
-		return 0;
+            }
 
-	}
+        } catch (IOException e) {
+            System.out.println("Error al leer del fichero");
+        } finally {
+            sc.close();
+        }
 
-	/**
-	* Lee de un fichero de texto los datos de los juegos
-	* con ayuda de un objeto de la  clase Scanner
-	* y los guarda en el array. 
-	*/
-	public void leerDeFichero() {
-		Scanner sc = null;
-		try {
-			sc = new Scanner(new File("juegos.txt"));
-			while (sc.hasNextLine()) {
-				Juego juego = new Juego(sc.nextLine());
-				this.add(juego);
-
-			}
-
-		} catch (IOException e) {
-			System.out.println("Error al leer del fichero");
-		} finally {
-			sc.close();
-		}
-
-	}
+    }
 
 }
